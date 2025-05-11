581a7a34b17c3cee0cfcdd823401a44844373b2f
Kostya Stern <kostiantyn.stern@oracle.com>
2025-05-12 00:23:40 +0300

    adding CoreFoundation dependency

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <CoreFoundation/CFString.h>
 
 using namespace std;
 
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!!!!"};
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     
     for (const string& word : msg)
     {
         cout << word << " ";
     }
     cout << endl;
+
+    CFStringRef utf8 = CFStringCreateWithCString(NULL, "Test string func", kCFStringEncodingUTF8);
+    int buflen = CFStringGetMaximumSizeOfFileSystemRepresentation(utf8);
+    cout << "buflen = " << buflen << endl;
 
     return 0;
 }
